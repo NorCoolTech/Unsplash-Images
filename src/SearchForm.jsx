afdde329d652949691fa281a00139d04f22ae5f7
Norbert Kulacin <norbert.kulacin@gmail.com>
2023-10-11 11:15:52 +0200

    serach form updated

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -20,7 +20,7 @@ const SearchForm = () => {
           type="text"
           className="form-input search-input"
           name="search"
-          placeholder="cat"
+          placeholder="dog"
         />
         <button type="submit" className="btn">
           search
