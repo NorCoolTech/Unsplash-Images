62738e2368fa493a59d8a01679f98b0be3497fc8
Norbert Kulacin <norbert.kulacin@gmail.com>
2023-10-17 09:39:05 +0200

    select options have been added

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -10,18 +10,25 @@ const url = `https://api.unsplash.com/search/photos?client_id=${
 const Gallery = () => {
   const { searchTerm } = useGlobalContext();
   const [randomMode, setRandomMode] = useState(false);
+  const [randomCount, setRandomCount] = useState(0);
 
   const response = useQuery({
-    queryKey: ["images", searchTerm, randomMode],
+    queryKey: ["images", searchTerm, randomMode, randomCount],
     queryFn: async () => {
       let apiUrl = url;
 
-      if (randomMode) {
+      if (randomMode && randomCount === 0) {
         apiUrl = `https://api.unsplash.com/photos/random/?count=10&client_id=${
           import.meta.env.VITE_API_KEY
         }`;
+      } else if (randomMode && randomCount > 0) {
+        apiUrl = `https://api.unsplash.com/photos/random/?count=${randomCount}&client_id=${
+          import.meta.env.VITE_API_KEY
+        }`;
       } else if (searchTerm) {
-        apiUrl += `&query=${searchTerm}`;
+        apiUrl = `https://api.unsplash.com/search/photos?client_id=${
+          import.meta.env.VITE_API_KEY
+        }&query=${searchTerm}`;
       }
 
       try {
@@ -71,6 +78,27 @@ const Gallery = () => {
         <button className="btn" onClick={() => setRandomMode(true)}>
           Get Random Images
         </button>
+        <select
+          name="randomImg"
+          id="randomImg"
+          value={randomCount}
+          onChange={(e) => {
+            setRandomCount(parseInt(e.target.value));
+            setRandomMode(true);
+          }}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+          <option value="20">20</option>
+          <option value="25">25</option>
+          <option value="30">30</option>
+          <option value="35">35</option>
+        </select>
         <button className="btn" onClick={() => setRandomMode(false)}>
           Clear Random
         </button>
